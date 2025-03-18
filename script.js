1cb6dc84d1e22423e49b55d2cde47d836767542b
shubh-codes7 <shubhamcodes7@gmail.com>
2025-03-18 10:24:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let str = ''
+for(let i=1; i<= 20; i++){
+	if(i%3 === 0 && i%5===0) str += "FizzBuzz\n"
+	else if(i%3 === 0) str += 'Fizz\n'
+	else if(i%5 === 0) str += "Buzz\n"
+	else str += (i+"\n")
+}
+alert(str)
